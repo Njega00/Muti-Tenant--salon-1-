e86d244194333024ed3619704773e4e9b6bae861
Lewis <lewisnjega120@gmail.com>
2026-06-23 16:38:35 +0300

    Add : Added a decode_token function that ensures that jwt token is decoded for auth

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -73,3 +73,5 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
+def decode_token(token: str) -> dict:
+    return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
